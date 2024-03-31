042ffe1578899c2fc6875d854c3659e1b132346e
落桜飘雪 <2714587763@qq.com>
2024-03-31 10:56:56 +0800

    polaris: overlay: enable GAPPS.

--- lineage_polaris.mk
+++ lineage_polaris.mk
@@ -10,6 +10,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 $(call inherit-product, device/xiaomi/polaris/device.mk)
 
+# Inherit Gapps
+$(call inherit-product, vendor/gapps/arm64/arm64-vendor.mk)
+
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
@@ -25,6 +28,7 @@ TARGET_GAPPS_ARCH := arm64
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_SUPPORTS_QUICK_TAP := true
 TARGET_USES_AOSP_RECOVERY := true
+TARGET_SUPPORTS_CALL_RECORDING := true
 
 PRODUCT_SYSTEM_NAME := polaris
 
