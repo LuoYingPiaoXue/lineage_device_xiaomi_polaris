46d0be8a8bed944038eec77370f215f76ea3e3ac
落桜飘雪 <2714587763@qq.com>
2024-03-31 10:52:20 +0800

    polaris: Pick Commit From PixelExperience's Device Tree.

--- lineage_polaris.mk
+++ lineage_polaris.mk
@@ -4,6 +4,10 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
 $(call inherit-product, device/xiaomi/polaris/device.mk)
 
 # Inherit some common Lineage stuff.
@@ -15,6 +19,12 @@ PRODUCT_DEVICE := polaris
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Mi MIX 2S
 PRODUCT_MANUFACTURER := Xiaomi
+
+# Inherit some common GAPPS stuff.
+TARGET_GAPPS_ARCH := arm64
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_USES_AOSP_RECOVERY := true
 
 PRODUCT_SYSTEM_NAME := polaris
 
